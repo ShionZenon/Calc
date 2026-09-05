687114c584dbf778e7e68d5a75de00a827ddd03f
ShionZenon <cc6789749@gmail.com>
2026-09-05 11:38:19 +0530

    Add files via upload

---
+++ n-.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    
+    cout << "Enter a number ";
+    cin >> num;
+    
+    if ( num > 0 ) {
+        cout <<  "Positive \n ";
+    } else if ( num < 0 ) {
+        cout << " Negative \n";
+    } else {
+        cout << " It is zero damn ";
+    }
+    return 0;
+}
+        
+   
+       
+        
